5a2ac5ad1150d552032d5b672be561ea7c8fee83
KunHwan Ahn <kunhwon@gmail.com>
2015-09-12 10:19:00 +0900

    git init

---
+++ src/com/ak/yellow/ribbon/locker/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.ak.yellow.ribbon.locker;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.ak.yellow.ribbon.locker.MainActivityTest \
+ * com.ak.yellow.ribbon.locker.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("com.ak.yellow.ribbon.locker", MainActivity.class);
+    }
+
+}
